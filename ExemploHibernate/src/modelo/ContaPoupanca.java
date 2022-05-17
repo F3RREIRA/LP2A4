f89040279b409574fec24a478aaa4af81f3f7d58
RIVAILDO <80759812+F3RREIRA@users.noreply.github.com>
2022-05-16 22:37:48 -0300

    Add files via upload

---
+++ ExemploHibernate/src/modelo/ContaPoupanca.java
@@ -0,0 +1,21 @@
+package modelo;
+
+import java.util.Date;
+
+public class ContaPoupanca extends ContaComum
+{
+	private Date aniversario;
+	
+	public ContaPoupanca(long numero, Date abertura)
+	{
+		super(numero, abertura);
+	}
+
+	public Date getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(Date aniversario) {
+		this.aniversario = aniversario;
+	}
+}
---
+++ ExemploHibernate/src/modelo/PessoaFisica.java
@@ -0,0 +1,42 @@
+package modelo;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "pessoasjuridicas")
+@PrimaryKeyJoinColumn(name= "id")
+public class PessoaFisica extends Pessoa
+{
+	@Column(nullable = false)
+	private long cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date nascto;
+	
+	public long getCpf()
+	{
+		return cpf;
+	}
+	
+	public void setCpf(long cpf)
+	{
+		this.cpf = cpf;
+	}
+	
+	public Date getNascto()
+	{
+		return nascto;
+	}
+	
+	public void setNascto(Date nascto)
+	{
+		this.nascto = nascto;
+	}
+}
